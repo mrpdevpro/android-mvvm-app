ff5e25a0c9b1249f38dd464af8e03520e833a6a4
devstackgr <devstackgr@gmail.com>
2017-02-05 02:40:36 +0200

    some unit tests

--- app/src/test/java/com/gumtree/adsdemo/addetails/domain/AdDetailDomainServiceDefaultTest.java
+++ app/src/test/java/com/gumtree/adsdemo/addetails/domain/AdDetailDomainServiceDefaultTest.java
@@ -1,15 +1,80 @@
 package com.gumtree.adsdemo.addetails.domain;
 
+import com.gumtree.adsdemo.RxImmediateSchedulerRule;
+import com.gumtree.adsdemo.addetails.net.AdDetailNetService;
+import com.gumtree.adsdemo.addetails.net.models.AdDetailsRestModel;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by marios on 5/2/2017.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class AdDetailDomainServiceDefaultTest {
+    AdDetailDomainService service;
+
+    @Rule
+    public RxImmediateSchedulerRule rxSchedulersTestRule = new RxImmediateSchedulerRule();
+
+    @Mock
+    private AdDetailNetService netService;
+
+    @Before
+    public void setUp() throws Exception{
+        service = new AdDetailDomainServiceDefault(netService);
+    }
     @Test
-    public void getDetail() throws Exception {
+    public void test_getDetail_Completes() throws Exception {
+        AdDetailsRestModel restModel = new AdDetailsRestModel();
+        restModel.setTitle("test");
+        when(netService.getAdDetails(anyString())).thenReturn(Observable.just(restModel));
+        TestSubscriber testSubscriber = new TestSubscriber();
+
+        service.getDetail(anyString()).
+                subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread()).
+                subscribe(testSubscriber);
+
+        testSubscriber.assertCompleted();
+        testSubscriber.assertNoErrors();
+
+        List<AdDetailModel> mode = testSubscriber.getOnNextEvents();
+        assertTrue(mode.get(0).getValue().getTitle().equals("test"));
+    }
+
+    @Test
+    public void test_getDetail_NetError() throws Exception {
+
+        Exception e = new Exception("error.net");
+        when(netService.getAdDetails(anyString())).thenReturn(Observable.error(e));
+        TestSubscriber testSubscriber = new TestSubscriber();
+
+        service.getDetail(anyString()).
+                subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread()).
+                subscribe(testSubscriber);
+
+        testSubscriber.assertNotCompleted();
+        testSubscriber.assertError(e);
 
     }
 
--- app/src/test/java/com/gumtree/adsdemo/addetails/viewmodel/AdDetailViewModelTest.java
+++ app/src/test/java/com/gumtree/adsdemo/addetails/viewmodel/AdDetailViewModelTest.java
@@ -1,22 +1,72 @@
 package com.gumtree.adsdemo.addetails.viewmodel;
 
+import com.gumtree.adsdemo.RxImmediateSchedulerRule;
+import com.gumtree.adsdemo.addetails.domain.AdDetailDomainService;
+import com.gumtree.adsdemo.addetails.domain.AdDetailModel;
+import com.gumtree.adsdemo.addetails.net.models.AdDetailsRestModel;
+
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import rx.Observable;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by marios on 5/2/2017.
  */
+@RunWith(MockitoJUnitRunner.class)
+
 public class AdDetailViewModelTest {
+    @Mock
+    AdDetailDomainService service;
+    @Rule
+    public RxImmediateSchedulerRule rxSchedulersTestRule = new RxImmediateSchedulerRule();
+
+    AdDetailViewModel viewModel;
+
     @Before
     public void setUp() throws Exception {
-
+        viewModel = new AdDetailViewModel(service);
     }
 
     @Test
-    public void start() throws Exception {
+    public void test_start_When_Return_Model_Loading_Stops() throws Exception {
+        AdDetailModel detailModel = new AdDetailModel(new AdDetailsRestModel());
+        detailModel.getValue().setPrice(1000);
 
+        when(service.getDetail(anyString())).thenReturn(Observable.just(detailModel));
+
+        viewModel.start(anyString());
+
+        assertTrue(!viewModel.loading.get());
+    }
+
+    @Test
+    public void test_start_When_Return_Model_With_Error_Loading_Stops() throws Exception {
+        when(service.getDetail(anyString())).thenReturn(Observable.error(new Exception()));
+
+        viewModel.start(anyString());
+
+        assertTrue(!viewModel.loading.get());
+    }
+    @Test
+    public void test_start_When_Return_Model_Populates_FormatedPrice() throws Exception {
+        AdDetailModel detailModel = new AdDetailModel(new AdDetailsRestModel());
+        detailModel.getValue().setPrice(1000);
+
+        when(service.getDetail(anyString())).thenReturn(Observable.just(detailModel));
+
+        viewModel.start(anyString());
+
+        assertTrue(viewModel.price.get().equals("£ 1000,00"));
     }
 
     @Test

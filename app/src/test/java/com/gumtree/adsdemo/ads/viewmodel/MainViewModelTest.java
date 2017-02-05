b399267ef59c46b20b75d915f1fde83eaf0894b7
mprdevpro <mariosprotos@gmail.com>
2017-02-05 02:50:56 +0200

    one more unit test

--- app/src/test/java/com/gumtree/adsdemo/ads/viewmodel/MainViewModelTest.java
+++ app/src/test/java/com/gumtree/adsdemo/ads/viewmodel/MainViewModelTest.java
@@ -1,16 +1,30 @@
 package com.gumtree.adsdemo.ads.viewmodel;
 
+import com.gumtree.adsdemo.ui.services.NavigationService;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 
-/**
- * Created by marios on 5/2/2017.
- */
+
+@RunWith(MockitoJUnitRunner.class)
 public class MainViewModelTest {
-    @Test
-    public void gotoDetailsCommand() throws Exception {
-
+    @Mock
+    NavigationService navigationService;
+    MainViewModel mainViewModel ;
+    @Before
+    public void setUp() throws Exception{
+        mainViewModel = new MainViewModel(navigationService);
     }
+    @Test
+    public void test_gotoDetailsCommand_Invokes_NavigationService() throws Exception {
+        mainViewModel.gotoDetailsCommand(anyString());
 
+        verify(navigationService).gotoDetails(anyString());
+    }
 }
